2ace841e55c4734eee8ad82c2ebfd1f3194d68c4
sina <sinarahbari@yahoo.com>
2023-08-19 22:34:13 +0330

    swiper middle created

--- main.js
+++ main.js
@@ -24,3 +24,28 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 // swiperTop end
+
+// swiper middle start
+var swiper = new Swiper(".Swipermiddle", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+// swipe middle end
