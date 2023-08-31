64a4690e61f20a68641f6f08cae1bbdc1df22824
sina <sinarahbari@yahoo.com>
2023-08-31 22:41:52 +0330

    MOSTPOPULAR create

--- main.js
+++ main.js
@@ -61,26 +61,36 @@ var swiper = new Swiper(".Swiper-2", {
 // swiper2 end
 
 // most popular start
-// var swiper = new Swiper(".SwipeRRR-3", {
-//   slidesPerView: 0,
-//   spaceBetween: 0,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     640: {
-//       slidesPerView: 2,
-//       spaceBetween: 0,
-//     },
-//     768: {
-//       slidesPerView: 4,
-//       spaceBetween: 0,
-//     },
-//     1024: {
-//       slidesPerView: 5,
-//       spaceBetween: 0,
-//     },
-//   },
-// });
+var swiper = new Swiper(".Swiper-3", {
+  slidesPerView: 9,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 8,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+    },
+  },
+});
 // most popular end
